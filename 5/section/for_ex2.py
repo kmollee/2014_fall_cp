3321db7de26e6328f1eaa2a3089e1bf74620c1fc
kmollee <10073105@gm.nfu.edu.tw>
2014-10-14 08:03:09 +0800

    fix word and value error

--- 5/section/for_ex2.py
+++ 5/section/for_ex2.py
@@ -4,5 +4,5 @@ Z = 3
 total = 0
 for num in range(X, Y + 1, Z):
     total = total + num
-assert total == 12
-print(X, "~", Y, "with", Z, "setp, total is", total)
+assert total == 22
+print(X, "~", Y, "with", Z, "step, total is", total)
