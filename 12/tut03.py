e066f0a85fb16a19cfdf9e651d21bd6d48ab8545
kmollee <10073105@gm.nfu.edu.tw>
2014-11-24 17:23:53 +0800

    add section week 12

---
+++ 12/tut03.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+import cherrypy
+
+
+class StringGenerator(object):
+
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+    @cherrypy.expose
+    def generate(self, length=8):
+        # string.hexdigits The string '0123456789abcdefABCDEF'.
+        return ''.join(random.sample(string.hexdigits, int(length)))
+
+if __name__ == '__main__':
+    cherrypy.quickstart(StringGenerator())
